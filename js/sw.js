ec64c4a5791c4da6d351d7e822196660857b3c9c
Nate <yt.nater.2017@gmail.com>
2023-09-23 14:37:16 -0700

    better service worker for site lockk

--- js/sw.js
+++ js/sw.js
@@ -1,19 +1,12 @@
 var allowedDomains = [
-  // PAID
   "https://nate-games.xyz",
-  // FREE
   "https://nate-games.github.io",
   "https://nate-games.netlify.app",
   "https://nate-games.vercel.app"
 ];
 
-var referringUrl = document.referrer;
+var currentDomain = window.location.origin;
 
-var isSitelock = referringUrl.indexOf("sitelock") !== -1;
-var isRootPath = window.location.pathname === "/";
-
-if (isSitelock && isRootPath) {
+if (allowedDomains.indexOf(currentDomain) === -1) {
   window.location.href = "https://nate-games.xyz/en-us/sitelock";
-} else {
-  // what else?
 }
