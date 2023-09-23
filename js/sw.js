0fceb8444b92251cfb1e0ce33d941a7f6b118a6d
Nate <yt.nater.2017@gmail.com>
2023-09-23 11:36:57 -0700

    test sitelock

---
+++ js/sw.js
@@ -0,0 +1,30 @@
+var allowedDomains = ['nate-games.github.io', 'nate-games.netlify.app', 'localhost:8080'];
+
+function getReferringDomain() {
+  var referrer = document.referrer;
+  if (!referrer) return null;
+
+  var match = referrer.match(/:\/\/(www\.)?([^/:]+)(:[0-9]+)?/i);
+  if (match && match.length > 2) {
+    return match[2].toLowerCase();
+  }
+
+  return null;
+}
+
+function isDomainAllowed() {
+  var referringDomain = getReferringDomain();
+  if (!referringDomain) return false;
+
+  for (var i = 0; i < allowedDomains.length; i++) {
+    if (referringDomain === allowedDomains[i]) {
+      return true;
+    }
+  }
+
+  return false;
+}
+if (!isDomainAllowed()) {
+  alert('Access to this site is not allowed from the current domain.');
+  window.location.href = 'https://nate-games.xyz/en-us/sitelock';
+}
