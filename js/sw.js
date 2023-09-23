7b36ff5b2561f6536afd6110a3d65dacf5748428
Nate <yt.nater.2017@gmail.com>
2023-09-23 14:11:55 -0700

    better service worker

--- js/sw.js
+++ js/sw.js
@@ -1,32 +1,25 @@
-var allowedDomains = ['nate-games.github.io', 'nate-games.netlify.app', 'cdn.nate-games.xyz', 'nate-games.xyz'];
+var allowedDomains = [
+  // PAID
+  "https://nate-games.com",
+  "https://nate-games.xyz",
+  // FREE
+  "https://nate-games.github.io",
+  "https://nate-games.netlify.app",
+  "https://nate-games.vercel.app"
+];
 
-function getReferringDomain() {
-  var referrer = document.referrer;
-  if (!referrer) return null;
+var referringUrl = document.referrer;
 
-  var match = referrer.match(/:\/\/(www\.)?([^/:]+)(:[0-9]+)?/i);
-  if (match && match.length > 2) {
-    return match[2].toLowerCase();
+var isSitelock = referringUrl.indexOf("sitelock") !== -1;
+
+if (isSitelock) {
+  window.location.href = "https://nate-games.xyz/en-us/sitelock";
+} else {
+  var isAllowedDomain = allowedDomains.some(function (domain) {
+    return referringUrl.indexOf(domain) !== -1;
+  });
+
+  if (!isAllowedDomain) {
+    window.location.href = allowedDomains[0];
   }
-
-  return null;
-}
-
-function isDomainAllowed() {
-  var referringDomain = getReferringDomain();
-  if (!referringDomain) return false;
-
-  var currentURL = window.location.href;
-  for (var i = 0; i < allowedDomains.length; i++) {
-    if (currentURL.indexOf(allowedDomains[i]) !== -1) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-if (!isDomainAllowed()) {
-  // alert('Access to this site is not allowed from the current domain.');
-  window.location.href = 'https://nate-games.xyz/en-us/sitelock';
 }
