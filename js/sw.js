ef10133fb0c28673875ba34878ca59e910a7e822
Nate <yt.nater.2017@gmail.com>
2023-09-24 11:44:17 -0700

    sw.js | allow google & cloudflare

--- js/sw.js
+++ js/sw.js
@@ -2,7 +2,9 @@ var allowedDomains = [
   "https://nate-games.xyz",
   "https://nate-games.github.io",
   "https://nate-games.netlify.app",
-  "https://nate-games.vercel.app"
+  "https://nate-games.vercel.app",
+  "https://google.com",
+  "https://cloudflare.com",
 ];
 
 var currentDomain = window.location.origin;
