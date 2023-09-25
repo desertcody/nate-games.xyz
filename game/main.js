87030ab71d447d5004c72dd04aeb931c32df86d7
Nate <yt.nater.2017@gmail.com>
2023-09-25 14:37:34 -0700

    add sdk to 1v1.lol

--- game/main.js
+++ game/main.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const queryString = window.location.search.substring(1);
 
     const pageMappings = {
-        "1v1.lol": "/0/g/1v1lol/",
+        "1v1.lol": "https://cdn.nate-games.xyz/src/play-now/?1v1.lol",
         "1v1.lol-method-2": "https://google-opensocial.googleusercontent.com/gadgets/ifr?url=https://cdn.jsdelivr.net/gh/ko1ov/edit@main/d3hjf73j2e92ge4y.xml",
         "justfall.lol": "/0/g/just-fall/",
         "cluster-rush": "/0/g/cluster-rush/",
