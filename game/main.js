ce613500f5f38a3785296f4681f25921d90247db
Nate <yt.nater.2017@gmail.com>
2023-09-27 16:06:32 -0700

    new hide bar option

--- game/main.js
+++ game/main.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     const queryString = window.location.search.substring(1);
 
-    const preloaderContainer = document.querySelector('.preloader-container');
     const iframe = document.getElementById("content");
     const playButton = document.getElementById("play-button");
     const gameImage = document.getElementById("game-img");
+
+    const gameThumbnail = document.getElementById("game-thumb");
 
     fetch('game-data.json')
         .then(response => response.json())
@@ -24,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 if (imageSrc) {
                     gameImage.src = imageSrc;
+                    gameThumbnail.src = imageSrc;
                 }
             } else {
                 changeText("Error fetching json");
@@ -43,12 +45,51 @@ function changeText(dynamicText) {
     document.getElementById('game-h3').textContent = dynamicText || 'Error fetching json';
 }
 
+function enterFullscreen() {
+    const iframe = document.querySelector('#embed-container');
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen();
+    }
+}
 
-
-// Bug Report
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'F' || event.key === 'f') {
+        enterFullscreen();
+    }
+});
 
 var currentURL = window.location.href;
 
-var linkElement = document.getElementById("bug.referer");
+var linkElement = document.getElementById("report-referer");
 
 linkElement.href += currentURL;
+
+
+function hidebar() {
+    const bar = document.querySelector('.bar')
+    const iframe = document.querySelector('#content')
+    const showbarButton = document.querySelector('.show-bar')
+    const fullscreenbutton = document.querySelector('#fullscreen-button')
+    fullscreenbutton.style.display = "none";
+    bar.style.display = "none";
+    iframe.style.height = "100%";
+    showbarButton.style.display = "block";
+
+}
+
+function showbar() {
+    const showbarButton = document.querySelector('.show-bar')
+    const bar = document.querySelector('.bar')
+    const iframe = document.querySelector('#content')
+    const fullscreenbutton = document.querySelector('#fullscreen-button')
+    iframe.style.height = "calc(100% - 50px)";
+    bar.style.display = "block";
+    fullscreenbutton.style.display = "block";
+    showbarButton.style.display = "none";
+}
