652c542d1bf7e0ab07f860cb13ce345b0d703d3a
Nate <yt.nater.2017@gmail.com>
2023-09-27 17:51:01 -0700

    update games/

--- js/sw.js
+++ js/sw.js
@@ -4,7 +4,8 @@ var allowedDomains = [
   "https://nate-games.netlify.app",
   "https://nate-games.vercel.app",
   "https://nates-cdn.pages.dev",
-  "http://192.168.4.63:8080"
+  "http://192.168.4.63:8080",
+  "http://localhost:8080"
 ];
 
 var currentDomain = window.location.origin;
@@ -12,6 +13,3 @@ var currentDomain = window.location.origin;
 if (allowedDomains.indexOf(currentDomain) === -1) {
   window.top.location.href = "https://nate-games.xyz/en-us/sitelock";
 }
-
-// LOCALHOST; ❎
-// COMPUTER IP; ✅
