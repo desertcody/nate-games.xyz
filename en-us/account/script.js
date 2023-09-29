653c809566340c0415819305849d8b71171207c4
Nate <yt.nater.2017@gmail.com>
2023-09-29 16:21:10 -0700

    add change username setting

--- en-us/account/script.js
+++ en-us/account/script.js
@@ -1,20 +1,29 @@
 const isAccountSaved = localStorage.getItem('accountSaved');
-const savedUsername = localStorage.getItem('username');
+let savedUsername = localStorage.getItem('username');
+const usernameElement = document.getElementById('username');
 
 if (isAccountSaved) {
-  if (savedUsername) {
-    const usernameElement = document.getElementById('username');
-    usernameElement.textContent = `${savedUsername}`;
-  }
+    if (savedUsername) {
+        usernameElement.textContent = `${savedUsername}`;
+    }
 } else {
-  window.location.href = '/en-us/signup/';
+    window.location.href = '/en-us/signup/';
 }
 
+document.getElementById("changeUsername").addEventListener("click", function () {
+    const newUsername = prompt("Enter your new username:");
+    if (newUsername !== null && newUsername !== "") {
+        localStorage.setItem('username', newUsername);
+        savedUsername = newUsername;
+        usernameElement.textContent = newUsername;
+    }
+});
+
 document.getElementById("revealLevel").addEventListener("click", function () {
-  var level = document.querySelector("#levelContent");
-  if (level.style.display === "none" || level.style.display === "") {
-    level.style.display = "block";
-  } else {
-    level.style.display = "none";
-  }
+    var level = document.querySelector("#levelContent");
+    if (level.style.display === "none" || level.style.display === "") {
+        level.style.display = "block";
+    } else {
+        level.style.display = "none";
+    }
 });
