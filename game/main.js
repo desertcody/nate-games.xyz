49de0c0b05dc2eb81e283ac81b831e9fc7c7aa8b
Nate <nathaniel_ramirez@icloud.com>
2023-10-09 14:33:09 -0700

    remove ingame ads

--- game/main.js
+++ game/main.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
   const loading = document.getElementById("loading-screen");
   const loadingText = document.getElementById("loading-text");
   const nateLogo = document.getElementById("nates-logo");
-  const adPreloader = document.getElementById("ad_preloader");
   const gameLoader = document.getElementById("game-loader");
 
   let iframeSrc = null;
@@ -42,63 +41,13 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => {
       console.error("Error fetching json data", error);
     });
-  function showAd() {
-    const IframeAd = document.getElementById("iframe_ad");
-    const adContent = document.getElementById("ad_content");
-    const adPreContent = document.getElementById("preload_ad-content");
-    const gameImg = document.getElementById("game-thumb").src;
-    const gameThumb = document.getElementById("ad_game-thumb");
-    const skipcountContainer = document.getElementById("skip_count-container");
-    gameThumb.src = gameImg;
-    adContent.style.display = "block";
-    const skipButton = document.getElementById("skipButton");
-    const skipCount = document.getElementById("skipCount");
-    const skipAllow = document.getElementById("skipAllow");
-    adPreContent.style.display = "none"; // https://www.w3schools.com/tags/av_event_canplaythrough.asp
-    const videoUrls = ["https://cdn.nate-games.xyz/center-ads"];
-    // https://stackoverflow.com/questions/63003226/randomly-changing-video-src-with-js
-    const randomIndex = Math.floor(Math.random() * videoUrls.length);
-    const randomVideoUrl = videoUrls[randomIndex];
-    IframeAd.src = randomVideoUrl;
-    skipButton.style.padding = "0";
-    setTimeout(() => {
-      skipButton.addEventListener("click", skipAd);
-      skipcountContainer.style.display = "none";
-      skipcountContainer.style.opacity = "0";
-      skipAllow.style.display = "block";
-      skipButton.style.padding = "10px 17px";
-    }, 3000);
-    setTimeout(() => {
-      skipCount.textContent = "2";
-    }, 1000);
-    setTimeout(() => {
-      skipCount.textContent = "1";
-    }, 2000);
-    setTimeout(skipAd, 10000); // https://www.w3schools.com/tags/av_prop_duration.asp
-  }
-  function skipAd() {
-    const IframeAd = document.getElementById("iframe_ad");
-    const continueButton = document.getElementById("continueButton");
-    const adContent = document.getElementById("ad_content");
-    // const adPreloader = document.getElementById("preload_ad-content")
-    adContent.style.display = "none";
-    // adPreloader.style.display = "block";
-    continueGame(); // Automatic goes to game instead of having to press the btn
-    continueButton.addEventListener("click", continueGame);
-    IframeAd.src = ""; // https://www.w3schools.com/tags/av_met_play.asp
-  }
-
-  function continueGame() {
-    iframe.src = iframeSrc;
-    iframe.style.display = "block";
-    iframe.style.zIndex = "1";
-  }
   playButton.addEventListener("click", function () {
     if (iframeSrc) {
       setTimeout(() => {
-        adPreloader.style.display = "block";
         loading.style.display = "none";
-        showAd();
+        iframe.src = iframeSrc;
+        iframe.style.display = "block";
+        iframe.style.zIndex = "1";
       }, 2000);
       setTimeout(() => {
         loadingText.textContent = "Preparing your game...";
@@ -118,143 +67,143 @@ document.addEventListener("DOMContentLoaded", function () {
       gameLoader.style.display = "none";
     }
   });
-});
 
-function changeText(dynamicText) {
-  document.getElementById("game-title").textContent =
-    dynamicText || "Error fetching json";
-  document.getElementById("game-h3").textContent =
-    dynamicText || "Error fetching json";
-}
-
-function changeDescription(dynamicText) {
-  document.getElementById("game-description").textContent =
-    dynamicText || "Play now on nate-games! 👇";
-}
-
-function enterFullscreen() {
-  const iframe = document.querySelector("#embed-container");
-
-  if (
-    document.fullscreenElement ||
-    document.webkitFullscreenElement ||
-    document.mozFullScreenElement ||
-    document.msFullscreenElement
-  ) {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen();
-    } else if (document.mozCancelFullScreen) {
-      document.mozCancelFullScreen();
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen();
-    }
-  } else {
-    if (iframe.requestFullscreen) {
-      iframe.requestFullscreen();
-    } else if (iframe.mozRequestFullScreen) {
-      iframe.mozRequestFullScreen();
-    } else if (iframe.webkitRequestFullscreen) {
-      iframe.webkitRequestFullscreen();
-    } else if (iframe.msRequestFullscreen) {
-      iframe.msRequestFullscreen();
-    }
+  function changeText(dynamicText) {
+    document.getElementById("game-title").textContent =
+      dynamicText || "Error fetching json";
+    document.getElementById("game-h3").textContent =
+      dynamicText || "Error fetching json";
   }
-}
 
-const fullscreenButton = document.querySelector("#fullscreen-button");
-fullscreenButton.addEventListener("click", enterFullscreen);
-
-document.addEventListener("keydown", function (event) {
-  if (event.key === "F" || event.key === "f") {
-    enterFullscreen();
+  function changeDescription(dynamicText) {
+    document.getElementById("game-description").textContent =
+      dynamicText || "Play now on nate-games! 👇";
   }
-});
 
-var currentURL = window.location.href;
+  function enterFullscreen() {
+    const iframe = document.querySelector("#embed-container");
 
-var linkElement = document.getElementById("report-referer");
-
-linkElement.href += currentURL;
-
-function hidebar() {
-  const Likes = document.querySelector(".likes");
-  const bar = document.querySelector(".bar");
-  const iframe = document.querySelector("#content");
-  const showbarButton = document.querySelector(".show-bar");
-  const fullscreenbutton = document.querySelector("#fullscreen-button");
-  fullscreenbutton.style.display = "none";
-  bar.style.display = "none";
-  iframe.style.height = "100%";
-  showbarButton.style.display = "block";
-  Likes.style.display = "none";
-}
-
-function showbar() {
-  const Likes = document.querySelector(".likes");
-  const showbarButton = document.querySelector(".show-bar");
-  const bar = document.querySelector(".bar");
-  const iframe = document.querySelector("#content");
-  const fullscreenbutton = document.querySelector("#fullscreen-button");
-  iframe.style.height = "calc(100% - 50px)";
-  bar.style.display = "block";
-  Likes.style.display = "block";
-  fullscreenbutton.style.display = "block";
-  showbarButton.style.display = "none";
-}
-
-function setSessionCookie(name, value) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const key = `${name}_${urlParams.toString()}`;
-  document.cookie = `${key}=${value};path=/`;
-}
-
-function getCookie(name) {
-  const urlParams = new URLSearchParams(window.location.search);
-  const key = `${name}_${urlParams.toString()}`;
-  const cookieName = `${key}=`;
-  const cookies = document.cookie.split(";");
-  for (const cookie of cookies) {
-    let c = cookie.trim();
-    if (c.startsWith(cookieName)) {
-      return c.substring(cookieName.length, c.length);
+    if (
+      document.fullscreenElement ||
+      document.webkitFullscreenElement ||
+      document.mozFullScreenElement ||
+      document.msFullscreenElement
+    ) {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      }
+    } else {
+      if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+      } else if (iframe.mozRequestFullScreen) {
+        iframe.mozRequestFullScreen();
+      } else if (iframe.webkitRequestFullscreen) {
+        iframe.webkitRequestFullscreen();
+      } else if (iframe.msRequestFullscreen) {
+        iframe.msRequestFullscreen();
+      }
     }
   }
-  return null;
-}
 
-const starred = document.getElementById("starred");
-const unstarred = document.getElementById("unstarred");
+  const fullscreenButton = document.querySelector("#fullscreen-button");
+  fullscreenButton.addEventListener("click", enterFullscreen);
 
-if (getCookie("starred") === "true") {
-  starred.style.display = "block";
-  unstarred.style.display = "none";
-} else {
-  starred.style.display = "none";
-  unstarred.style.display = "block";
-}
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "F" || event.key === "f") {
+      enterFullscreen();
+    }
+  });
 
-starred.addEventListener("click", function () {
-  if (starred.style.display === "none") {
-    unstarred.style.display = "none";
+  var currentURL = window.location.href;
+
+  var linkElement = document.getElementById("report-referer");
+
+  linkElement.href += currentURL;
+
+  function hidebar() {
+    const Likes = document.querySelector(".likes");
+    const bar = document.querySelector(".bar");
+    const iframe = document.querySelector("#content");
+    const showbarButton = document.querySelector(".show-bar");
+    const fullscreenbutton = document.querySelector("#fullscreen-button");
+    fullscreenbutton.style.display = "none";
+    bar.style.display = "none";
+    iframe.style.height = "100%";
+    showbarButton.style.display = "block";
+    Likes.style.display = "none";
+  }
+
+  function showbar() {
+    const Likes = document.querySelector(".likes");
+    const showbarButton = document.querySelector(".show-bar");
+    const bar = document.querySelector(".bar");
+    const iframe = document.querySelector("#content");
+    const fullscreenbutton = document.querySelector("#fullscreen-button");
+    iframe.style.height = "calc(100% - 50px)";
+    bar.style.display = "block";
+    Likes.style.display = "block";
+    fullscreenbutton.style.display = "block";
+    showbarButton.style.display = "none";
+  }
+
+  function setSessionCookie(name, value) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = `${name}_${urlParams.toString()}`;
+    document.cookie = `${key}=${value};path=/`;
+  }
+
+  function getCookie(name) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const key = `${name}_${urlParams.toString()}`;
+    const cookieName = `${key}=`;
+    const cookies = document.cookie.split(";");
+    for (const cookie of cookies) {
+      let c = cookie.trim();
+      if (c.startsWith(cookieName)) {
+        return c.substring(cookieName.length, c.length);
+      }
+    }
+    return null;
+  }
+
+  const starred = document.getElementById("starred");
+  const unstarred = document.getElementById("unstarred");
+
+  if (getCookie("starred") === "true") {
     starred.style.display = "block";
-    setSessionCookie("starred", "true");
+    unstarred.style.display = "none";
   } else {
     starred.style.display = "none";
     unstarred.style.display = "block";
-    setSessionCookie("starred", "");
   }
-});
 
-unstarred.addEventListener("click", function () {
-  if (unstarred.style.display === "none") {
-    starred.style.display = "none";
-    unstarred.style.display = "block";
-    setSessionCookie("starred", "");
-  } else {
-    unstarred.style.display = "none";
-    starred.style.display = "block";
-    setSessionCookie("starred", "true");
-  }
+  starred.addEventListener("click", function () {
+    if (starred.style.display === "none") {
+      unstarred.style.display = "none";
+      starred.style.display = "block";
+      setSessionCookie("starred", "true");
+    } else {
+      starred.style.display = "none";
+      unstarred.style.display = "block";
+      setSessionCookie("starred", "");
+    }
+  });
+
+  unstarred.addEventListener("click", function () {
+    if (unstarred.style.display === "none") {
+      starred.style.display = "none";
+      unstarred.style.display = "block";
+      setSessionCookie("starred", "");
+    } else {
+      unstarred.style.display = "none";
+      starred.style.display = "block";
+      setSessionCookie("starred", "true");
+    }
+  });
 });
