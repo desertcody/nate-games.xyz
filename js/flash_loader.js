0b880923820bce659cba444561121f34f1748325
Nate <nathaniel_ramirez@icloud.com>
2023-10-14 09:50:35 -0700

    add fullscreen buttonto flash opener

--- js/flash_loader.js
+++ js/flash_loader.js
@@ -10,7 +10,14 @@ function handleFileChange(event) {
   }
 }
 
+function openFileInput() {
+  var fileInput = document.getElementById("file-input");
+  fileInput.click();
+}
 function loadFlash() {
+  const fullscreenContainer = document.getElementById("fullscreen-container");
+  const fullscreenButton = document.getElementById("fullscreen-flash");
+  fullscreenButton.style.display = "block";
   var fileInput = document.getElementById("file-input");
   var file = fileInput.files[0];
 
@@ -20,16 +27,59 @@ function loadFlash() {
       var flashContent = event.target.result;
 
       var container = document.getElementById("flash-container");
+
+      fullscreenContainer.style.width = "1200px";
+      fullscreenContainer.style.height = "625px";
+      container.style.width = "100%";
+      container.style.height = "100%";
       container.innerHTML = "";
 
       var embed = document.createElement("embed");
       embed.src = flashContent;
       embed.type = "application/x-shockwave-flash";
-      embed.width = "1200px";
-      embed.height = "625px";
+      embed.width = "100%";
+      embed.height = "100%";
 
       container.appendChild(embed);
     };
     reader.readAsDataURL(file);
   }
 }
+
+function fullscreenFlash() {
+  const iframe = document.querySelector("#fullscreen-container");
+
+  if (
+    document.fullscreenElement ||
+    document.webkitFullscreenElement ||
+    document.mozFullScreenElement ||
+    document.msFullscreenElement
+  ) {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+  } else {
+    if (iframe.requestFullscreen) {
+      iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) {
+      iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) {
+      iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) {
+      iframe.msRequestFullscreen();
+    }
+  }
+}
+window.RufflePlayer.config = {
+  autoplay: "on",
+  unmuteOverlay: "hidden",
+  contextMenu: false,
+  preloader: false,
+  warnOnUnsupportedContent: false,
+};
