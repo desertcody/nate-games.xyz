c7c08455e91036c0dfa833eba00c2b3af00341f8
Nate <nathaniel_ramirez@icloud.com>
2023-10-16 19:07:52 -0700

    better settings

--- js/editor.js
+++ js/editor.js
@@ -58,3 +58,15 @@ if (tabData.title) {
 if (tabData.icon) {
   setFavicon(tabData.icon);
 }
+function openBlank() {
+  var win = window.open();
+  var url = window.origin;
+  var iframe = win.document.createElement("iframe");
+  win.document.body.style.margin = "0";
+  win.document.body.style.padding = "0";
+  iframe.style.width = "100%";
+  iframe.style.height = "100%";
+  iframe.style.border = "0";
+  iframe.src = url;
+  win.document.body.appendChild(iframe);
+}
